56b66d065e107d7d0eb302e7e8107d1bddf81e55
alireza-badrloo-dev <alireza.badrloo.dev@gmail.com>
2026-06-11 18:31:52 +0330

    recyclerView With TimePickerDialog Original

--- app/src/main/java/com/example/myapplication6/NameViewHolder.java
+++ app/src/main/java/com/example/myapplication6/NameViewHolder.java
@@ -15,46 +15,19 @@ import androidx.recyclerview.widget.RecyclerView;
 public class NameViewHolder extends RecyclerView.ViewHolder {
     private ImageView image_item;
     private TextView name_item;
-    private TextView  more_item;
     private TextView time_item;
 
     public NameViewHolder(@NonNull View itemView) {
         super(itemView);
         image_item = itemView.findViewById(R.id.image_item);
         name_item = itemView.findViewById(R.id.name_item);
-        more_item = itemView.findViewById(R.id.more_item);
         time_item = itemView.findViewById(R.id.time_item);
     }
 
     public void setItems(String name , int imageId){
         name_item.setText(name);
         image_item.setImageResource(imageId);
-        more_item.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alertdialog = new AlertDialog.Builder(v.getContext());
-                alertdialog.setTitle("تست تایتل");
-                alertdialog.setMessage("تست پیام");
 
-                alertdialog.setPositiveButton("بله", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                alertdialog.setNegativeButton("بله", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-
-
-                alertdialog.setCancelable(false);
-                alertdialog.show();
-
-            }
-        });
 
         time_item.setOnClickListener(new View.OnClickListener() {
             @Override
