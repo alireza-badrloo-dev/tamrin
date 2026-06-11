d66582a5055c94c08fcdaf8ae90e4f78869741e9
alireza-badrloo-dev <alireza.badrloo.dev@gmail.com>
2026-06-11 18:21:21 +0330

    recyclerView with TimePickerDialog

--- app/src/main/java/com/example/myapplication6/NameViewHolder.java
+++ app/src/main/java/com/example/myapplication6/NameViewHolder.java
@@ -1,10 +1,12 @@
 package com.example.myapplication6;
 
 import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,14 +16,14 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
     private ImageView image_item;
     private TextView name_item;
     private TextView  more_item;
-
+    private TextView time_item;
 
     public NameViewHolder(@NonNull View itemView) {
         super(itemView);
         image_item = itemView.findViewById(R.id.image_item);
         name_item = itemView.findViewById(R.id.name_item);
         more_item = itemView.findViewById(R.id.more_item);
-
+        time_item = itemView.findViewById(R.id.time_item);
     }
 
     public void setItems(String name , int imageId){
@@ -30,7 +32,7 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
         more_item.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder alertdialog = new AlertDialog.Builder(itemView.getContext());
+                AlertDialog.Builder alertdialog = new AlertDialog.Builder(v.getContext());
                 alertdialog.setTitle("تست تایتل");
                 alertdialog.setMessage("تست پیام");
 
@@ -51,6 +53,19 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
                 alertdialog.setCancelable(false);
                 alertdialog.show();
 
+            }
+        });
+
+        time_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                    }
+                },23 , 01 ,true);
+                timePickerDialog.show();
             }
         });
     }
