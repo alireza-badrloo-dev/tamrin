63734f58259fbf4ab3b9a06fdc611837eef8e61e
alireza-badrloo-dev <alireza.badrloo.dev@gmail.com>
2026-06-17 16:53:20 +0330

    base project

--- app/src/main/java/com/example/myapplication6/MainActivity.java
+++ app/src/main/java/com/example/myapplication6/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.myapplication6;
 
-
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-import android.view.View;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager2 viewPager2 = findViewById(R.id.viewpager2);
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
 
-        viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
-        viewPager2.setAdapter(viewPagerAdapter);
 
     }
 }
